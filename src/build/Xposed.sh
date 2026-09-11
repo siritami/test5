a3a2ad6015844e6f6d408f9a2a3304dfdbab923c
Fioren <102145692+FiorenMas@users.noreply.github.com>
2026-09-11 21:10:35 +0700

    fix

--- src/build/Xposed.sh
+++ src/build/Xposed.sh
@@ -41,7 +41,7 @@ patch_dl(){
 	dl_gh "NexAlloy" "gnadgnaoh" "v1.1"
 	# Patch Zalo:
 	get_apk "com.zing.zalo" "zalo" "bundle" "arm64-v8a + armeabi-v7a"
-	lspatch "zalo" "NexAlloy*.apk" "gnadgnaoh"
+	npatch "zalo" "NexAlloy*.apk" "gnadgnaoh"
 }
 5() {
 	NPatch_dl
