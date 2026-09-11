3c4287a9cb02e5b894355c409d4da8eaa1304c80
Fioren <102145692+FiorenMas@users.noreply.github.com>
2026-09-11 21:07:14 +0700

    fix

--- src/build/Xposed.sh
+++ src/build/Xposed.sh
@@ -37,7 +37,7 @@ patch_dl(){
 	lspatch "threads-arm64-v8a" "NexAlloy-nonroot*.apk" "gnadgnaoh" "--injectdex --sigbypasslv 3"
 }
 4() {
-	NPatch_dl
+	dl_gh "NPatch" "7723mod" "v1.0.5"
 	dl_gh "NexAlloy" "gnadgnaoh" "v1.1"
 	# Patch Zalo:
 	get_apk "com.zing.zalo" "zalo" "bundle" "arm64-v8a + armeabi-v7a"
