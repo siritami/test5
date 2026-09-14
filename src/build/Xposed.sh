bf0a22f71f2254db133e3a87cd27dbb318673be2
siritami <103488094+siritami@users.noreply.github.com>
2026-09-15 06:53:23 +0700

    Update Xposed.sh

--- src/build/Xposed.sh
+++ src/build/Xposed.sh
@@ -2,49 +2,49 @@
 # Xposed build
 source ./src/build/utils.sh
 
-NPatch_dl(){
-	dl_gh "NPatch" "7723mod" "latest"
+LSPatch_dl(){
+	dl_gh "LSPatch" "JingMatrix" "latest"
 }
 patch_dl(){
 	dl_gh "NexAlloy" "gnadgnaoh" "v1.0"
 }
 1() {
 	# Patch Revenge:
-	NPatch_dl
+	LSPatch_dl
 	dl_gh "revenge-xposed" "revenge-mod" "latest"
 	get_apk "com.discord" "discord" "bundle"
 	npatch "discord" "app-release" "revenge"
 }
 2() {
-	NPatch_dl
+	LSPatch_dl
 	patch_dl
 	# Patch Facebook:
 	version="577.0.0.50.72"
 	get_apk "com.facebook.katana" "facebook-arm64-v8a" "bundle" "arm64-v8a" "160-640dpi" "Android 11+"
 	npatch "facebook-arm64-v8a" "NexAlloy-nonroot*.apk" "gnadgnaoh" "--sigbypasslv 3"
+	# Patch Messenger:
+	get_apk "com.facebook.orca" "messenger-arm64-v8a" "apk" "arm64-v8a" "nodpi" "Android 9.0+"
+	npatch "messenger-arm64-v8a" "NexAlloy-nonroot*.apk" "gnadgnaoh" "--sigbypasslv 3"
 }
 3() {
-	NPatch_dl
+	LSPatch_dl
 	patch_dl
 	# Patch Instagram:
 	get_apk "com.instagram.android" "instagram-arm64-v8a" "bundle" "arm64-v8a" "120-640dpi"  "Android 9.0+"
 	npatch "instagram-arm64-v8a" "NexAlloy-nonroot*.apk" "gnadgnaoh" "--sigbypasslv 3"
-}
-4() {
-	dl_gh "test5" "siritami" "latest"
-	patch_dl
-	# Patch Zalo:
-	get_apkpure "com.zing.zalo" "zalo" "bundle"
-	npatch "zalo" "NexAlloy*.apk" "gnadgnaoh" "--sigbypasslv 3"
-	# Patch Messenger:
-	get_apk "com.facebook.orca" "messenger-arm64-v8a" "apk" "arm64-v8a" "nodpi" "Android 9.0+"
-	npatch "messenger-arm64-v8a" "NexAlloy-nonroot*.apk" "gnadgnaoh" "--sigbypasslv 3"
 	# Patch Thread:
 	get_apk "com.instagram.barcelona" "threads-arm64-v8a" "bundle" "arm64-v8a" "320-480dpi" "Android 9.0+"
 	npatch "threads-arm64-v8a" "NexAlloy-nonroot*.apk" "gnadgnaoh" "--sigbypasslv 3"
 }
+4() {
+	LSPatch_dl
+	patch_dl
+	# Patch Zalo:
+	get_apkpure "com.zing.zalo" "zalo" "bundle"
+	npatch "zalo" "NexAlloy*.apk" "gnadgnaoh" "--sigbypasslv 3"
+}
 5() {
-	NPatch_dl
+	LSPatch_dl
 	patch_dl
 	# Patch Tiktok:
 	get_apk "com.zhiliaoapp.musically" "tiktok" "apk"
